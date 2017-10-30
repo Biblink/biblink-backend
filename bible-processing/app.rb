514baf4ac52bb9f4f1038cd53038745a1886df1f
Brandon Fan <brandonfan1256@gmail.com>
2017-10-30 12:05:04 -0400

    add books.txt to filter and create a hash for bible

--- bible-processing/app.rb
+++ bible-processing/app.rb
@@ -1,5 +1,24 @@
 require 'bible_parser'
-bible = BibleParser.new(File.open('../english-web-bible.xml'))
-bible.verses.each do |verse|
-    print verse.text
+bible = BibleParser.new(File.open('../bible-files/english-web-bible.xml'))
+books = File.readlines('../bible-files/books.txt')
+books.each do |book|
+    book.strip!
+end
+bible_data = []
+bible.books.each do |book|
+    if books.include? book.to_s
+        puts book
+        book_data = {'name' => book, 'data' => []}
+        book.chapters.each do |chapter|
+            puts chapter
+            # append to a hash of each book
+            chapter_data = {'chapter' => chapter, 'verses' => []}
+            chapter.verses.each do |verse|
+                verse_data = {'verse_number' => verse, 'text' => verse.text}
+                chapter_data['verses'].push(verse_data)
+            end
+            book_data['data'].push(chapter_data)
+        end
+        bible_data.push(book_data)
+    end
 end
