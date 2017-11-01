ed97662ed21c17a7788fe7a6e5e367ee2e3215de
JDS0530 <33209724+JDSeiler@users.noreply.github.com>
2017-10-31 22:54:22 -0400

    Added server code

--- server/server.py
+++ server/server.py
@@ -1,15 +1,25 @@
-from flask import Flask, request
+import random
+from flask import Flask, request, jsonify
 from bible_functions import Bible
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/query')
 def api_root():
+    rand_num = random.randint(1, 1000000)
+    response = {'query_id': rand_num, 'url': request.url}
     query = request.args.get('query')
-    Bible(query)
-    matched_query = query.parse_query 
-    data = matched_query.get_data 
-    return jsonify(data) 
+    response['query'] = query
+    instance = Bible('../bible-files/english-web-bible.json')
+    matched_query = instance.parse_query(query)
+    results_list = []
+    for temp_data in matched_query['queries']:
+        temp_var = instance.get_data(matched_query['book'],
+                                     temp_data['chapter'],
+                                     temp_data['verses'])
+        results_list.append(temp_var)
+    response['data'] = results_list
+    return jsonify(response)
 
 
 if __name__ == '__main__':
