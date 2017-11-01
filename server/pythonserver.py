229d277380d7e65f4114893c906a0ab12b9803af
JDS0530 <33209724+JDSeiler@users.noreply.github.com>
2017-10-31 21:57:42 -0400

    Changed name to python server and began adding functions.

---
+++ server/pythonserver.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, jsonify
+from bible_functions import Bible
+app = Flask(__name__)
+
+
+@app.route('/')
+def api_root():
+    query = request.args.get('query')
+    Bible(query)
+    matched_query= query.parse_query 
+    data = matched_query.get_data 
+    return jsonify(data) 
+
+if __name__ == '__main__':
+    app.run(port='5000')
