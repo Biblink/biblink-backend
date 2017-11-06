9010869d2ed2dee7bec9f873a91d03f9030b5327
Brandon Fan <brandonfan1256@gmail.com>
2017-11-05 21:04:05 -0500

    fix pylint problems

--- server/similarity_functions/similarity.py
+++ server/similarity_functions/similarity.py
@@ -1,22 +1,31 @@
 """Similarity Class to handle verse similarities
 Authors: Brandon Fan
 Last Edit Date: 11/5/2017
-Todo:
-    * implement proper documentation and docstrings
 """
 
 import os
+import string
 import json
-import nltk
+import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import string
 import gensim
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
 
 class Similarity(object):
+    """Similarity class that utilizes deep learning to find similar verses
+
+    Uses gensim Doc2Vec and scikit-learn TF-IDF to calculate similar verses
+
+    Attributes:
+        bible_file (str): Bible file path to load JSON file.
+        use_nltk (bool | optional): Whether or not to use nltk processing
+    """
+
     def __init__(self, bible_file, use_nltk=False):
-        if bible_file.endswith('.json'):
+        if bible_file.endswith('.json') and os.path.isfile(bible_file):
             self.bible_data = json.load(
                 open(bible_file, encoding='utf-8-sig'))
             self.verse_data = []
@@ -29,13 +38,21 @@ class Similarity(object):
 
         self.stopwords_list = set(stopwords.words('english'))
         self.exclude = set(string.punctuation)
-        self.tagged_docs = list(self._create_corpus(self.verse_data, use_nltk=use_nltk=False))
+        self.tagged_docs = list(self.create_corpus(
+            self.verse_data, use_nltk=use_nltk))
         self.model = gensim.models.Doc2Vec.load('doc2vec.model')
         self.tokenize_data(self.verse_data)
 
-    def _tokenize_data(self, verse_data):
+    def tokenize_data(self, verse_data):
+        """Tokenizes passed in verse data
+        Uses nltk word_tokenize to tokenize sentences into words
+
+        Attributes:
+            verse_data (dict): dictionary of verse data
+        """
         for verse in verse_data:
-            text = ''.join(ch for ch in verse['text'] if ch not in exclude)
+            text = ''.join(
+                ch for ch in verse['text'] if ch not in self.exclude)
             tokenized_text = word_tokenize(text)
             final_text = []
             for val in tokenized_text:
@@ -49,30 +66,46 @@ class Similarity(object):
     def _throw_value_error(information):
         raise ValueError(information)
 
-    def _create_corpus(self, verse_tokenized_data, use_nltk=False):
-        for verse in self.verse_data:
+    @staticmethod
+    def create_corpus(verse_tokenized_data, use_nltk=False):
+        """Creates corpus of tagged documents based off of tokenized data
+        Uses either nltk or gensim.utils
+
+        Attributes:
+            verse_tokenized_data (dict): dictionary of verse data
+            use_nltk (bool | optional): whether or not to use nltk
+        """
+        for verse in verse_tokenized_data:
             if use_nltk:
                 words = verse['tokenized_text']
                 yield gensim.models.doc2vec.TaggedDocument(words, [verse['verse']])
             else:
                 words = verse['text']
-                yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(words), [verse['verse']])
+                yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(words),
+                                                           [verse['verse']])
 
     def get_similar_values(self, text, total_values=10):
+        """Gets similar values using Doc2Vec and TF-IDF
+        Uses Doc2Vec and TF-IDF combined to produce certain amount of
+        similar bible verses
+
+        Attributes:
+            text (str): verse text to find similar values
+            total_values (int | optional): number of values to return
+        """
         inferred_vector = self.model.infer_vector(text)
         most_similar = self.model.docvecs.most_similar(
             [inferred_vector], topn=total_values + 10)
-        text = [''.join(tokenized_text)]
+        text = [''.join(text)]
         verses = []
         for verse_name, _ in most_similar:
-            for verse in verse_data:
+            for verse in self.verse_data:
                 if verse['verse'] == verse_name:
                     text.append(verse['text'])
                     verses.append(verse['verse'])
         vectorizer = TfidfVectorizer(stop_words='english')
         tf_idf_transform = vectorizer.fit_transform(text)
-        cos_sim_matrix = cosine_similarity(tf_idf_transform)
-        sim_text = cos_sim_matrix[0][1:]
+        sim_text = cosine_similarity(tf_idf_transform)[0][1:]
         final_text = []
         final_indices = list(reversed(np.argsort(sim_text)))[:10]
         for index in final_indices:
