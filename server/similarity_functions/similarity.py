b7120523a3052f5aeecaef8137d61de2e15269ce
Brandon Fan <brandonfan1256@gmail.com>
2017-11-12 23:35:03 -0500

    edit glove scripts and add functionality to similarity

--- server/similarity_functions/similarity.py
+++ server/similarity_functions/similarity.py
@@ -6,13 +6,14 @@ Last Edit Date: 11/5/2017
 import os
 import string
 import json
+import csv
+import pandas as pd
 import numpy as np
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
-import gensim
-from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
+MAX_LEN = 24
 
 class Similarity(object):
     """Similarity class that utilizes deep learning to find similar verses
@@ -24,30 +25,35 @@ class Similarity(object):
         use_nltk (bool | optional): Whether or not to use nltk processing
     """
 
-    def __init__(self, bible_file, use_nltk=False):
-        if bible_file.endswith('.json') and os.path.isfile(bible_file):
+    def __init__(self, bible_file, glove_file):
+        if self._check_file(bible_file, '.json'):
             self.bible_data = json.load(
                 open(bible_file, encoding='utf-8-sig'))
             self.verse_data = []
         else:
-            self._throw_value_error('Please enter a proper json file')
+            self._throw_value_error('Please enter a proper bible .json file')
+        if self._check_file(glove_file, '.txt'):
+            self.glove_words = pd.read_table(glove_file,
+                                             sep=" ", index_col=0,
+                                             header=None, quoting=csv.QUOTE_NONE)
+        else:
+            self._throw_value_error('Please enter a proper glove .txt file')
         for book in self.bible_data:
             for chapter in book['data']:
                 for verses in chapter['verses']:
                     self.verse_data.append(verses)
-
+        # preprocess text corpus
         self.stopwords_list = set(stopwords.words('english'))
         self.exclude = set(string.punctuation)
-        self.tagged_docs = list(self.create_corpus(
-            self.verse_data, use_nltk=use_nltk))
-        self.model = gensim.models.Doc2Vec.load('doc2vec.model')
         self.tokenize_data(self.verse_data)
+        self.verse_data = self.convert_to_glove_vectors(self.verse_data)
+        self.sim_matrix = cosine_similarity([verse['vector'] for verse in self.verse_data])
 
     def tokenize_data(self, verse_data):
         """Tokenizes passed in verse data
         Uses nltk word_tokenize to tokenize sentences into words
 
-        Attributes:
+        Args:
             verse_data (dict): dictionary of verse data
         """
         for verse in verse_data:
@@ -62,52 +68,96 @@ class Similarity(object):
                     final_text.append(val)
             verse['tokenized_text'] = final_text
 
+    def convert_to_glove_vectors(self, verse_data):
+        """Converts verse tokenized text into GloVe vectors
+        Uses GloVe embeddings to convert verse tokenized text
+        into vectors of size 200
+
+        Args:
+            verse_data (dict): verse_data run after tokenized_data
+        
+        Returns:
+            verse_data_with_glove (dict): verse_data with GloVe vectors
+            response is below::
+                {
+                    'verse': (str),
+                    'verse_number': (str),
+                    'text': (str),
+                    'tokenized_text': (list),
+                    'vector': (list)
+                }
+        """
+        verse_data_with_glove = []
+        for verse in verse_data:
+            vector = np.array([])
+            for word in verse['tokenized_text'][:MAX_LEN]:
+                try:
+                    vector = np.append(vector, self.get_glove_vector(word))
+                except KeyError:
+                    vector = np.append(vector, np.zeros(200))
+            if vector.shape[0] < MAX_LEN * 200:
+                vector = np.append(vector, np.zeros([MAX_LEN * 200 - vector.shape[0]]))
+            verse['vector'] = vector
+            verse_data_with_glove.append(verse)
+        return verse_data_with_glove
+
+    def get_glove_vector(self, word):
+        """Grabs GloVe vector representation of word
+        Uses GloVe embeddings to reference word vector
+
+        Args:
+            word (str): word to find vector for
+        
+        Returns:
+            (list): the GloVe representation of word
+        
+        Raises:
+            ValueError: if word is not found in embeddings
+        """
+        try:
+            return self.glove_words.loc[word].as_matrix()
+        except KeyError:
+            self._throw_value_error('{0} was not found in the glove embeddings.'.format(word))
+
+    def get_similar_values(self, verse, total_values=10):
+        """Gets similar values using GloVe and Cosine Sim.
+        Uses GloVe embeddings and cosine similarity matrix to find
+        similar bible verses
+
+        Args:
+            verse (str): verse reference (i.e. 'Genesis 1:1' or 'Exodus 2:3')
+            total_values (int | optional): number of values to return
+        
+        Returns:
+            final_text (list): list of similar verses
+            response is below::
+                [
+                    {
+                        'verse': (str),
+                        'verse_number': (str),
+                        'text': (str),
+                        'tokenized_text': (list),
+                        'vector': (list)
+                    }
+                    ...
+                ] (length of list = total_values)
+        """
+        proper_index = None
+        for index, verse_text in enumerate(self.verse_data):
+            if verse_text['verse'] == verse:
+                proper_index = index
+                break
+        sim_text = self.sim_matrix[proper_index][1:]
+        final_text = []
+        final_indices = list(reversed(np.argsort(sim_text)))[:total_values]
+        for index in final_indices:
+            final_text.append(self.verse_data[index])
+        return final_text
+
+    @staticmethod
+    def _check_file(file_path, ending):
+        return file_path.endswith(ending) and os.path.isfile(file_path)
+
     @staticmethod
     def _throw_value_error(information):
         raise ValueError(information)
-
-    @staticmethod
-    def create_corpus(verse_tokenized_data, use_nltk=False):
-        """Creates corpus of tagged documents based off of tokenized data
-        Uses either nltk or gensim.utils
-
-        Attributes:
-            verse_tokenized_data (dict): dictionary of verse data
-            use_nltk (bool | optional): whether or not to use nltk
-        """
-        for verse in verse_tokenized_data:
-            if use_nltk:
-                words = verse['tokenized_text']
-                yield gensim.models.doc2vec.TaggedDocument(words, [verse['verse']])
-            else:
-                words = verse['text']
-                yield gensim.models.doc2vec.TaggedDocument(gensim.utils.simple_preprocess(words),
-                                                           [verse['verse']])
-
-    def get_similar_values(self, text, total_values=10):
-        """Gets similar values using Doc2Vec and TF-IDF
-        Uses Doc2Vec and TF-IDF combined to produce certain amount of
-        similar bible verses
-
-        Attributes:
-            text (str): verse text to find similar values
-            total_values (int | optional): number of values to return
-        """
-        inferred_vector = self.model.infer_vector(text)
-        most_similar = self.model.docvecs.most_similar(
-            [inferred_vector], topn=total_values + 10)
-        text = [''.join(text)]
-        verses = []
-        for verse_name, _ in most_similar:
-            for verse in self.verse_data:
-                if verse['verse'] == verse_name:
-                    text.append(verse['text'])
-                    verses.append(verse['verse'])
-        vectorizer = TfidfVectorizer(stop_words='english')
-        tf_idf_transform = vectorizer.fit_transform(text)
-        sim_text = cosine_similarity(tf_idf_transform)[0][1:]
-        final_text = []
-        final_indices = list(reversed(np.argsort(sim_text)))[:10]
-        for index in final_indices:
-            final_text.append({'verse': verses[index], 'text': text[index]})
-        return final_text
