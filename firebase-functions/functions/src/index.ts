f169d24bf833f55108c461544bb03efd8ea8062f
JDSeiler <33209724+JDSeiler@users.noreply.github.com>
2018-03-17 21:42:16 -0400

    added firebase function for updating leader names

--- firebase-functions/functions/src/index.ts
+++ firebase-functions/functions/src/index.ts
@@ -1,8 +1,56 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import { PassThrough } from 'stream';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const adminAccount =  require('../admin_key.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(adminAccount),
+    databaseURL: 'https://biblya-ed2ec.firebaseio.com/'
+});
+
+const db = admin.firestore()
+
+exports.updateLeaderName = functions.firestore.document('users/{userId}').onUpdate((event) => {
+    
+    const updatedValue = event.data.data();
+    const name = updatedValue.name;
+    const userId = event.params.userId;
+    
+    const userStudies = db.collection('users')
+                        .doc(userId)
+                        .collection('studies')
+                        .where('role', '==', 'leader')
+                        .get()
+                        .then(snapshot => {
+                            let studyIds = [];
+                            snapshot.forEach(doc => {
+                                studyIds.push(doc.id)
+                            });
+                            return studyIds
+                        })
+                        .catch(err => {
+                            console.log(err)
+                            return undefined;
+                        });
+   
+    return userStudies.then(studyIds => {
+        if (studyIds == undefined)
+            return 'person is not a leader in any studies';
+        else {
+           const studiesRef = db.collection('studies')
+           studyIds.forEach(ID => {
+                const metadata = studiesRef.doc(ID).get().then(snapshot => {
+                    let studyMeta = snapshot.data()['metadata']
+                    studyMeta['leader'] = name;
+                    const updataMetaData = studiesRef.doc(ID).update({metadata: studyMeta})
+               })
+           })
+           return `updated studies with leader: ${name}`;
+        }
+
+
+    })
+    
+                
+})
